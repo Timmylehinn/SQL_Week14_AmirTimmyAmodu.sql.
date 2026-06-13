0bbdf3a0a496ecd37cb9bfe2cefacce6fb361f38
Timmylehinn <amiramodu@gmail.com>
2026-06-13 14:05:37 +0100

    Delete SQL_Week14_AmirTimmyAmodu.sql

--- SQL_Week14_AmirTimmyAmodu.sql
+++
@@ -1,2 +0,0 @@
-# SQL_Week14_AmirTimmyAmodu.sql.
-I Created two related tables and establish foreign key relationships, using JOIN queries to merge data across tables. Perform aggregation operations to summarize data. Finally, I practice updating and deleting records.
